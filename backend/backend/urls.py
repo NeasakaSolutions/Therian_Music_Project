e55e4da21e3937c2f95ff0822b5b58ef0624c255
Ubaldo Narvaez <satou.matsuzaka.3443@gmail.com>
2026-04-18 17:10:49 -0600

    Se termino de configurar la api de prueba

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,8 @@ urlpatterns = [
     path('', include("home.urls")),
     path('api/mai/', include("uwu.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
