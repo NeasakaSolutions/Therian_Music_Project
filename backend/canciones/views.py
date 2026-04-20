129fd613d58a7bd15c32832022493bde2f238da3
Ubaldo Narvaez <satou.matsuzaka.3443@gmail.com>
2026-04-20 13:13:27 -0600

    Se agrego el metodo post a la vista de canciones

--- backend/canciones/views.py
+++ backend/canciones/views.py
@@ -9,7 +9,10 @@ from http import HTTPStatus
 from django.http import Http404
 from django.utils.text import slugify
 from django.utils.dateformat import DateFormat
+from datetime import datetime
 from canciones.models import Cancion
+from categorias.models import Categoria
+from artistas.models import Artista
 from canciones.serializers import CancionSerializer
 
 # Clase sin argumentos:
@@ -23,6 +26,81 @@ class CancionesLista(APIView):
         return JsonResponse({
             "data": datos_json.data
         })
+    
+    # Agregar campo
+    def post(self, request):
+
+        # Validaciones:
+        if request.data.get("nombre") == None or not request.data.get("nombre"):
+            return JsonResponse({
+                "estado": "error",
+                "mensaje": "El campo nombre es obligatorio"
+            }, status = HTTPStatus.BAD_REQUEST)
+        
+        if request.data.get("descripcion") == None or not request.data.get("descripcion"):
+            return JsonResponse({
+                "estado": "error",
+                "mensaje": "El campo descripcion es obligatorio"
+            }, status = HTTPStatus.BAD_REQUEST)
+        
+        if request.data.get("categoria_id") == None or not request.data.get("categoria_id"):
+            return JsonResponse({
+                "estado": "error",
+                "mensaje": "El campo categoria es obligatorio"
+            }, status = HTTPStatus.BAD_REQUEST)
+        
+        if request.data.get("artista_id") == None or not request.data.get("artista_id"):
+            return JsonResponse({
+                "estado": "error",
+                "mensaje": "El campo artista es obligatorio"
+            }, status = HTTPStatus.BAD_REQUEST)
+        
+        # Validar que exista la categoria:
+        try:
+            categoria = Categoria.objects.filter(id = request.data["categoria_id"]).get()
+
+        except Categoria.DoesNotExist:
+            return JsonResponse({
+                "estado": "error",
+                "mensaje": "La categoria ingresada no existe"
+            }, status = HTTPStatus.BAD_REQUEST)
+        
+        # Validar que exista el artista:
+        try:
+            artista = Artista.objects.filter(id = request.data["artista_id"]).get()
+
+        except Artista.DoesNotExist:
+            return JsonResponse({
+                "estado": "error",
+                "mensaje": "El artista ingresado no existe"
+            }, status = HTTPStatus.BAD_REQUEST)
+
+        # Validar que el nombre de la cancion no se repita:
+        if Cancion.objects.filter(nombre = request.data.get("nombre")).exists():
+            return JsonResponse({
+                "estado": "error",
+                "mensaje": "El nombre de la cancion ya existe"
+            }, status = HTTPStatus.BAD_REQUEST)
+
+        # Crear campo
+        try:
+            Cancion.objects.create(
+                nombre = request.data.get("nombre"),
+                descripcion = request.data.get("descripcion"),
+                categoria_id = request.data.get("categoria_id"),
+                artista_id = request.data.get("artista_id"),
+                fecha = datetime.now(),
+                foto = "sss",
+                cancion = "ssss"
+            )
+
+            return JsonResponse({
+                "estado": "ok",
+                "mensaje": "Se creo el registro correctamente"
+            }, status = HTTPStatus.CREATED)
+
+        except Exception as e:
+            raise Http404
     
 # Clase con  argumentos:
 class CancionDetalle(APIView):
